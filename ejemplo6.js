75f1a81932628a2e9c34861ce9d8ac879c36f1f7
Juan <alonso.jad@gmail.com>
2024-05-29 10:48:49 +0200

    añadidos ejercicios 7 y 8

--- ejemplo6.js
+++ ejemplo6.js
@@ -7,47 +7,55 @@ function mostrarRecetas(evt) {
     const contenedorRecetas = document.getElementById('contenedorRecetas');
     contenedorRecetas.textContent = "";
     for (const receta of peticion.response.recipes) {
-        const divReceta = document.createElement("div");
-        const nombre = document.createElement("h2");
-        nombre.classList.add("text-center");
-        nombre.textContent = receta.name;
-        divReceta.appendChild(nombre);
-        const dificultad = document.createElement("p");
-        dificultad.textContent = "Dificultad: " + receta.difficulty;
-        divReceta.appendChild(dificultad);
+        const divReceta = crearReceta(receta);
 
         const divListas = document.createElement("div");
         divListas.classList.add("d-flex", "justify-content-around");
         divReceta.appendChild(divListas);
         
-        let ul = document.createElement("ul");
-        for (const ingrediente of receta.ingredients) {
-            const li = document.createElement("li");
-            li.textContent = ingrediente;
-            ul.appendChild(li);
-        }
+        let ul = crearLista(receta.ingredients);
         divListas.appendChild(ul);
 
-        ul = document.createElement("ul");
-        for (const instruccion of receta.instructions) {
-            const li = document.createElement("li");
-            li.textContent = instruccion;
-            ul.appendChild(li);
-        }
+        ul = crearLista(receta.instructions);
         divListas.appendChild(ul);
-
-        const img = document.createElement("img");
-        img.setAttribute("src", receta.image);
-        img.setAttribute("width", 200);
-        img.setAttribute("alt", receta.name);
-        img.classList.add("d-block", "m-auto");
-        divReceta.insertBefore(img, nombre);
 
         divReceta.classList.add("m-2","p-2", "bg-danger-subtle");
         contenedorRecetas.appendChild(divReceta);
     }
 }
 
+function crearReceta(receta) {
+    const divReceta = document.createElement("div");
+    const nombre = document.createElement("h2");
+    nombre.classList.add("text-center");
+    nombre.textContent = receta.name;
+    divReceta.appendChild(nombre);
+    const dificultad = document.createElement("p");
+    dificultad.textContent = "Dificultad: " + receta.difficulty;
+    divReceta.appendChild(dificultad);
+    const img = crearImagenReceta(receta);
+    divReceta.insertBefore(img, nombre);
+    return divReceta;
+}
+
+function crearImagenReceta(receta) {
+    const img = document.createElement("img");
+    img.setAttribute("src", receta.image);
+    img.setAttribute("width", 200);
+    img.setAttribute("alt", receta.name);
+    img.classList.add("d-block", "m-auto");
+    return img;
+}
+
+function crearLista(elementos) {
+    let ul = document.createElement("ul");
+    for (const elemento of elementos) {
+        const li = document.createElement("li");
+        li.textContent = elemento;
+        ul.appendChild(li);
+    }
+    return ul;
+}
 
 function mostrarError() {
     window.alert("Se ha producido un error en la petición");
