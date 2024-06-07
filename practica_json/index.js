5ce52766ac6fd3d78603a63085278b846e2818d9
Juan <alonso.jad@gmail.com>
2024-06-07 10:38:38 +0200

    Añadidos comentarios al js del ejercicio practica_json

--- practica_json/index.js
+++ practica_json/index.js
@@ -1,3 +1,8 @@
+/* Tenemos un array de objetos con:
+ - texto: el valor que el usuario debe buscar en el JSON
+ - fichero: fichero que se cargará para la pregunta. Lo cargaremos localmente mediante AJAX
+ - correcta: el resultado en texto que debe salir para que la respuesta del usuario sea correcta
+ */
 let preguntas = [
     {texto: "nombre", fichero: "j1.json", correcta: "Juan Pérez"},
     {texto: "email", fichero: "j1.json", correcta: "juan.perez@ejemplo.com"},
@@ -38,55 +43,92 @@ let preguntas = [
     {texto: "el nombre del segundo miembro del primer equipo del primer proyecto", fichero: "j4.json", correcta: 'Pedro'},
 ];
 
+// Esta variable contendrá la pregunta actual y que servirá para obtenerla del array preguntas.
+// Como los arrays comienzan en la posición cero, ese es el valor que le damos
 let preguntaActual = 0;
-let datos = "";
 
+/**
+ * Muestra el json del ejercicio y la pregunta
+ * @param {JSON} json - objeto JSON a mostrar al usuario
+ */
 function mostrarJSON(json) {
     const pregunta =  document.getElementById("pregunta");
+    // Contenido es un textarea
     const contenido =  document.getElementById("contenido");
+    /* Usamos el metodo JSON.stringify para convertir el objeto JSON en una cadena que
+    podemos mostrar en el textarea.
+    Para poder mostrarlo bien formateado indicamos un tercer parámetro con el número
+    de espacios a tabular lso datos*/
     contenido.value = JSON.stringify(json, null, 3);
-    datos = json;
+    // Mostramos la pregunta actual
     pregunta.textContent = preguntas[preguntaActual].texto;
 }
 
+/**
+ * Realiza una petición AJAX para cargar el fichero JSON de la pregunta actual
+ */
 function obtenerJSON() {
     fetch(preguntas[preguntaActual].fichero)
-    .then(peticion => peticion.json())
+    // Este es una función arrow que veremos más adelante. 
+    // Simplente devuelve los datos convertidos a un objeto JSON y se pasan al siguiente then
+    .then(peticion => peticion.json()) 
     .then(mostrarJSON);
 }
 
+/**
+ * Comprueba si la respuesta es correcta, en cuyo caso pasa a la siguiente pregunta
+ */
 function comprobar() {
+    // Con trim quitamos los espacios vacíos a los lados que peuda haber puesto el usuario
     const valor = document.getElementById("texto").value.trim();
     if(valor == "") {
         alert("Falta el texto");
         return;
     }
+    /* En este elemento mostraremos los datos a partir de lo indicado por el usuario */
     const obtenido = document.getElementById("obtenido");
+    /* Como vamos a usar la función eval para ejecutar código JavaScript, esta puede 
+    dar un error si no es código JavaScript válido. Por ello debemos meter en un try
+    el código que puede fallar y el que debe seguir ejecutándose en caso de que no haya error
+    Luego en el catch pondremos lo que hacer en caso de error. */
     try {
+        /* La función eval coge un texto con código JavaScript y lo ejecuta. En este
+        caso queremos usarlo para obtener los datos JSON que diga el usuario.
+        Es una función peligrosa si no tenemos 100% certeza de que ese código JavaScript
+        es seguro y nuestro */
         const evaluado = eval(valor);
-        if(typeof(evaluado) == "object") {
+        /* evall puede devolver un texto con el valor (por ejemplo el país, nombre, ...)
+         o un objeto JSON con varios datos (como un producto, persona, ...). 
+         Por ello debemos actuar diferente según el caso */
+        if(typeof(evaluado) == "object") { // Si es un objeto lo convertimos a texto
             obtenido.textContent= JSON.stringify(evaluado);
         } else {
+            // Si ya es un texto lo mostramos tal cual
             obtenido.textContent= evaluado;
         }
-        console.log(obtenido.textContent);
+        // Comprobamos si lo obtenido es igual a la pregunta correcta 
         if(obtenido.textContent == preguntas[preguntaActual].correcta) {
             siguientePregunta();
         } else {
+            // Si falla añadimos una nueva imagen a los fallos
             const fallos = document.getElementById("fallos");
             fallos.innerHTML += "<img src='fallo.png'>";
         }
     } catch {
+        // eval puede producir una excepción si lo introducido no es código JavaScript válido
         obtenido.textContent = "Incorrecto";
     }
 }
 
 function siguientePregunta() {
-    preguntaActual++;
+    preguntaActual++; // Pasamos a la siguiente pregunta
+    // Vemos si ya hemos llegado a la última pregunta
     if(preguntaActual == preguntas.length) {
         alert("Finalizado");
+        // Desactivamos el botón comprobar
         document.getElementById("comprobar").disabled = true;
     } else {
+        // Obtenemos el json de la siguiente pregunta
         obtenerJSON();
         document.getElementById("obtenido").textContent = "";
         const texto = document.getElementById("texto");
@@ -95,13 +137,18 @@ function siguientePregunta() {
     }
 }
 
+/**
+ * Pulsando la tecla Enter se llamará a la función comprobar salvo que ya no queden preguntas
+ * @param {Event} evt - información del evento
+ */
 function comprobarEnter(evt) {
     const tecla = evt.key;
-    if(tecla == "Enter") {
+    if(tecla == "Enter" && preguntaActual < preguntas.length) {
         comprobar();
     }
 }
 
+// Como queremos mostrar el JSON al comienzo, llamamos a la función fuera de cualquier evento
 obtenerJSON();
 document.getElementById("texto").addEventListener("keydown", comprobarEnter);
 document.getElementById("comprobar").addEventListener("click", comprobar);
